622011ecdef2398a88ebaf1e580b4bd3eda513d5
CHEN HUO <huoc@MacChen.local>
2011-10-15 00:45:18 -0500

    haha

--- enhancedRRDTest.py
+++ enhancedRRDTest.py
@@ -2,23 +2,7 @@ import os, sys
 from record import Record  #This is chen's file, should be included in the dir
 from optparse import OptionParser
 import conf
-from updater import * 
-
-#ARG_MAX = os.popen('getconf ARG_MAX').read()
-
-#def createTestRRDDb(dbname, step):
-#
-#	command = 	'rrdtool create ' + dbname + ' '\
-#				'--start N ' + \
-#				'--step ' + str(step) + ' '
-#	
-#				'DS:bw:GAUGE:'+str(step*5)+':0:U '+ \
-#				'RRA:AVERAGE:0.5:1:500 ' + \
-#				'RRA:AVERAGE:0.5:5:500 ' + \
-#				'RRA:AVERAGE:0.5:10:500'
-#				
-#	print command
-#	os.popen(command)
+from updater import *  #This is chen's file, should be included in the dir
 
 def updateTestRRDDb(dbname, r, items):
 	command =	'rrdtool update ' + dbname + ' '\
@@ -37,29 +21,38 @@ def _optParser(parser):
 						help = 'Specify a name for the rrd db.\t[default=%default]')
 	parser.add_option("-s", "--step", dest = "step", default = '1000',
 						help = 'Specify the step length\n'+\
-						'NOTE: Step is suggested to be at least longer than your actual test interval\t[default=%default]')
+						'For <create>\t[default=%default]')
 	parser.add_option("-b", "--start", dest = "start", default = '-500000',
 						help = 'start time to plot. default is 50000 sec before now\t[default=%default]')
 	parser.add_option("-e", "--end", dest = "end", default = 'N',
 						help = 'end time to plot. default is NOW\t[default=%default]')
 	parser.add_option("-t", "--from", dest = "interval", default = 'no',
-						help = "most helpful time interval selection.e.g. -t lastmonth see or change choices in conf.py")
+						help = "for <plot>&<plotall> most helpful time interval selection."+\
+						"e.g. -t lastmonth see or change choices in conf.py")
 	parser.add_option("-p", "--protocol", dest = "protocol", default = 'tcp',
-						help = "Tcp or udp?\t[default=%default]")
+						help = "For <dump> tcp or udp?\t[default=%default]")
 	return parser.parse_args()
 
 
 def main():
-	usage = '''IMPORTANT: record.py must be in the directory
-		#1 python simpleRRDTest.py create [-d databasename] [-s steplenthofrrddb]
-		#2 (get sth on stdout) | python simpleRRDTest.py update [-d databasename]
-		e.g.:   cat testouput | python simpleRRDTest.py update
-		#3 python simpleRRDTest.py plot [[-b begintime] [-e endtime]|[-t timeinterval] [-d databasename]
+	usage = '''
+			python enhancedRRDTest.py [create|update|plot|plotall|dump] [options]
+			Example1:
+				python enhancedRRDTest.py create -d test.rrd --step 250
+				while doing the bwctl test:
+					cat bwctloutput | python enhancedRRDTest.py update -d test.rrd
+				python enhancedRRDTest.py plot -d test.rrd -t lastweek
+			Example2: Dump from a mysqldb. (customized by conf.py)
+				python enhancedRRDTest.py dump -p tcp
+				python enhancedRRDTest.py plotall -t lastday
+				python enhancedRRDTest.py plotall --start 1313000000 --end 1313130000
 			'''
 	
 	p = OptionParser(usage=usage)
 	(options, args) = _optParser(p)
-	print options, args
+	if len(args) == 0: 
+		print 'try: python <scriptname> -h'
+		sys.exit(1)
 	if args[0] == 'create': createTestRRDDb(options.dbname, options.step, conf._items)
 	elif args[0] == 'update': 
 		r = readFromSout()
@@ -70,7 +63,6 @@ def main():
 		for pairs in permutateHosts(conf._hosts):
 			rrdname = pairs[0].split('.')[0] + pairs[1].split('.')[0]+'.rrd'
 			plotTestRRDDb(options.start, options.end, rrdname, options.interval, conf._items)
-			print 'One round'
 		os.popen('scp *.png huoc@student.cs.uni.edu:~/')
 	elif args[0] == 'dump': 
 		dumpFromMySQL(conf._items, options.step, options.protocol)
